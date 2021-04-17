8b67f4aa5d294abe1168e2bca98314ad20cfc574
Dan Levy <danlevy.ca@icloud.com>
2021-04-16 21:43:49 -0400

    Added multiplexer and fixing blynk.

--- SmartGarden.ino
+++ SmartGarden.ino
@@ -6,21 +6,41 @@
 ****************************************************/
 
 /* PINOUT */
-#define dhtPin 5
-#define tdsPin A2
+#define dhtPin D5
+#define tdsPin A0
 #define moisture1Pin A0
-#define moisture2Pin A1
-#define ldr1Pin A3
-#define ldr2Pin A4
+#define moisture2Pin A0
+#define ldr1Pin A0
+#define ldr2Pin A0
+#define muxOutput = A0;
+const int muxControls[3] = {D0, D1, D2};
 /* END PINOUT */
+
+/* Multiplexer */
+#define nbAnalogique 4
+const int muxChannels[5][3] = {
+    {0, 0, 0}, // Channel 0
+    {1, 0, 0}, // Channel 1
+    {0, 1, 0}, // Channel 2
+    {1, 1, 0}, // Channel 3
+    {0, 0, 1}  // Channel 4
+};
+float (*func[])(){
+    getMoisture1,
+    getMoisture2,
+    getLDR1,
+    getLDR2};
+float data[4];
+/* END Multiplexer */
 
 /* Blynk Setup*/
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-char auth[] = "_Vgk1QOn3lLHxauDHBVv-R-Zk3-FEHXU";
-char ssid[] = "cavabienaller";
-char pass[] = "PRIVELEVY";
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
 BlynkTimer timer;
+const int virtualPin[nbAnalogique] = {V0, V1, V2, V3};
 /* END Blynk */
 
 /* DHT11 sensor */
@@ -51,14 +71,6 @@ float ldr1;
 float ldr2;
 /* END LDR */
 
-/* Blynk Configuration */
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-char auth[] = "REVIEW:";
-char ssid[] = "REVIEW:";
-char pass[] = "REVIEW:";
-/* END Blynk */
-
 void setup()
 {
     Serial.begin(9600);
@@ -72,8 +84,15 @@ void setup()
 
     /* Blynk Setup */
     Blynk.begin(auth, ssid, pass, "192.168.1.100", 8080);
-    timer.setInterval(1000L, getData());
+    timer.setInterval(1000L, getData);
     /* END Blynk */
+
+    /* Mux setup */
+    for (int i = 0; i < 3; i++)
+    {
+        pinMode(muxControls[i], OUTPUT);
+    }
+    /* END Mux */
 }
 
 void loop()
@@ -84,22 +103,57 @@ void loop()
 
 void getData()
 {
-    float h = getHum();
-    float t = getTemp();
-    float tds = getTds();
-    float m1 = getMoisture1();
-    float m2 = getMoisture2();
-    float l1 = getLDR1();
-    float l2 = getLDR2();
-    Blynk.virtualWrite(V5, h);
-    Blynk.virtualWrite(V6, t);
+    for (int i = 0; i < nbAnalogique; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            digitalWrite(muxControls[j], muxChannels[i][j]);
+        }
+
+        data[i] = (func[i])();
+        Blynk.virtualWrite(virtualPin[i], data[i]);
+    }
+
+    humidity = getHum();
+    temperature = getTemp();
+    Blynk.virtualWrite(V4, humidity);
+    Blynk.virtualWrite(V5, temperature);
+
+    for (int j = 0; j < 3; j++)
+    {
+        digitalWrite(muxControls[j], muxChannels[nbAnalogique][j]);
+    }
+    tds = getTds(temperature);
+    Blynk.virtualWrite(V6, tds);
 }
 
 /**
-  * Get value from TDS sensor in ppm.
-  *
-  * @param temperature Temperature at which measure was taken.
-  * @return Tds value of sensor in ppm.
+    Get humidity from DHT11 sensor.
+
+    @return Humidity from DHT11 in %.
+*/
+float getTemp()
+{
+    Dht.read(dhtPin);              // Retreive value
+    return (float)Dht.temperature; // Return humidity
+}
+
+/**
+    Get temperature from DHT11 sensor.
+
+    @return Temperature from DHT11 in °C.
+*/
+float getHum()
+{
+    Dht.read(dhtPin);           // Retreive value
+    return (float)Dht.humidity; // Return humidity
+}
+
+/**
+    Get value from TDS sensor in ppm.
+
+    @param temperature Temperature at which measure was taken.
+    @return Tds value of sensor in ppm.
 */
 float getTds(float temperature)
 {
@@ -109,31 +163,9 @@ float getTds(float temperature)
 }
 
 /**
-  * Get humidity from DHT11 sensor.
-  *
-  * @return Humidity from DHT11 in %.
-*/
-void getTemp()
-{
-    Dht.read(dhtPin);              // Retreive value
-    return (float)Dht.temperature; // Return humidity
-}
+    Get soil moisture from sensor 1 in percent.
 
-/**
-  * Get temperature from DHT11 sensor.
-  *
-  * @return Temperature from DHT11 in °C.
-*/
-void getHum()
-{
-    Dht.read(dhtPin);           // Retreive value
-    return (float)Dht.humidity; // Return humidity
-}
-
-/**
-  * Get soil moisture from sensor 1 in percent.
-  *
-  * @return Soil moisture from sensor 1 in %.
+    @return Soil moisture from sensor 1 in %.
 */
 float getMoisture1()
 {
@@ -142,16 +174,16 @@ float getMoisture1()
         moisture1 += analogRead(moisture1Pin);
         delay(1);
     }
-    moisture1 = moisture1 / 100.0 // Get average value
-                // Map value between 0 and 100 to get a percentage
-                moisture1 = map(moisture1, MOISTURE_AIR, MOISTURE_WATER, 0, 100);
+    moisture1 = moisture1 / 100.0; // Get average value
+    // Map value between 0 and 100 to get a percentage
+    moisture1 = map(moisture1, MOISTURE_AIR, MOISTURE_WATER, 0, 100);
     return constrain(moisture1, 0, 100); // Constrain value inside [0, 100]
 }
 
 /**
-  * Get soil moisture from sensor 2 in percent.
-  *
-  * @return Soil moisture from sensor 2 in %.
+    Get soil moisture from sensor 2 in percent.
+
+    @return Soil moisture from sensor 2 in %.
 */
 float getMoisture2()
 {
@@ -160,40 +192,42 @@ float getMoisture2()
         moisture2 += analogRead(moisture2Pin);
         delay(1);
     }
-    moisture2 = moisture2 / 100.0 // Get average value
-                // Map value between 0 and 100 to get a percentage
-                moisture2 = map(moisture2, MOISTURE_AIR, MOISTURE_WATER, 0, 100);
+    moisture2 = moisture2 / 100.0; // Get average value
+    // Map value between 0 and 100 to get a percentage
+    moisture2 = map(moisture2, MOISTURE_AIR, MOISTURE_WATER, 0, 100);
     return constrain(moisture2, 0, 100); // Constrain value inside [0, 100]
 }
 
 /**
-  * Get light intensity from sensor 1 in percent.
-  *
-  * @return Light intensity from sensor 1 in %.
+    Get light intensity from sensor 1 in percent.
+
+    @return Light intensity from sensor 1 in %.
 */
 float getLDR1()
 {
     for (int i = 0; i <= 100; i++)
     {
         ldr1 += analogRead(ldr1Pin);
-        delay(1)
+        delay(1);
     }
-    ldr1 = ldr1 / 100.0 ldr1 = map(ldr1, LDR_MIN, LDR_MAX, 0, 100);
+    ldr1 = ldr1 / 100.0;
+    ldr1 = map(ldr1, LDR_MIN, LDR_MAX, 0, 100);
     return constrain(ldr1, 0, 100);
 }
 
 /**
-  * Get light intensity from sensor 2 in percent.
-  *
-  * @return Light intensity from sensor 2 in %.
+    Get light intensity from sensor 2 in percent.
+
+    @return Light intensity from sensor 2 in %.
 */
 float getLDR2()
 {
     for (int i = 0; i <= 100; i++)
     {
         ldr2 += analogRead(ldr2Pin);
-        delay(1)
+        delay(1);
     }
-    ldr2 = ldr2 / 100.0 ldr2 = map(ldr2, LDR_MIN, LDR_MAX, 0, 100);
+    ldr2 = ldr2 / 100.0;
+    ldr2 = map(ldr2, LDR_MIN, LDR_MAX, 0, 100);
     return constrain(ldr2, 0, 100);
 }
